52e12b0b3035c0234eb8739d09e21061377e30a3
Adrian Radores <aradores@repay.ph>
2025-09-18 16:23:30 +0800

    feat: created custom directive to make text bigger

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,16 @@
 import './bootstrap';
+
+Livewire.directive('bigger-text', ({ el, directive, component, cleanup }) => {
+    let content =  directive.expression
+
+    let onClick = e => {
+        e.target.className = 'text-4xl'
+    }
+
+    el.addEventListener('click', onClick, { capture: true })
+
+    cleanup(() => {
+        el.removeEventListener('click', onClick)
+    })
+})
+
