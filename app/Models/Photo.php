e543ce26d8dfe7b48ac158bedb70d840c87a8a6b
Adrian Radores <aradores@repay.ph>
2025-09-18 15:57:56 +0800

    feat(photo): create models,factory and migration for photos

---
+++ app/Models/Photo.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Photo extends Model
+{
+
+    protected $fillable = [
+        'title',
+        'description',
+        'image',
+    ];
+
+    /** @use HasFactory<\Database\Factories\PhotoFactory> */
+    use HasFactory;
+}
